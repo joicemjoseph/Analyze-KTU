678d5701c7b6b0c8b3be5a58abda93c8a3be7245
Joice <josukutty@outlook.com>
2017-09-16 12:03:40 +0530

    fix for #7. updates setup.sh to update the repositories. moved scripts to scripts/ directory

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,7 @@ if [ $EUID -ne 0 ]; then
     error "This script should not be run using sudo or as the root user"
     exit 1
 fi
+apt-get update
 apt-get install git mysql-server mysql-client python python-pip vim
 pip install --upgrade pip
 pip install virtualenv virtualenvwrapper
